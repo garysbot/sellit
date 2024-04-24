434cf6b91cb7457243728b428df5c18792605d8a
Gary <g@modernhuman.xyz>
2024-04-24 18:52:17 -0400

    playing with Image core component

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { SafeAreaView } from 'react-native-web';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
   const handlePress = () => {
@@ -9,15 +8,14 @@ export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.text} numberOfLines={10} onPress={handlePress}>
-        Hello World! It's Gary Jiang. In the flesh.
-        Hello World! It's Gary Jiang. In the flesh.
-        Hello World! It's Gary Jiang. In the flesh.
-        Hello World! It's Gary Jiang. In the flesh.
-        Hello World! It's Gary Jiang. In the flesh.
-        Hello World! It's Gary Jiang. In the flesh.
-      </Text>
+      <Text style={styles.text} onPress={handlePress}>Hello World! It's Gary Jiang. In the flesh.</Text>
       <Text style={styles.text}>Hi there. The mobile view renders so fast.</Text>
+      <Image 
+        source={{ 
+          uri: "https://picsum.photos/200/300", 
+          width: 200,
+          height: 300
+        }} />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
