8408f4cf3a5fc2263a7a46f66b75c51c620dcd15
Gary <g@modernhuman.xyz>
2024-04-24 18:45:28 -0400

    playing with core components

--- App.js
+++ App.js
@@ -1,22 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-web';
 
 export default function App() {
-  console.log(`Hello World`)
+  const handlePress = () => {
+    console.log('Text pressed')
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World! It's Gary Jiang. In thFe flesh.</Text>
-      <Text>Hi there. The mobile view renders so fast.</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text} numberOfLines={10} onPress={handlePress}>
+        Hello World! It's Gary Jiang. In the flesh.
+        Hello World! It's Gary Jiang. In the flesh.
+        Hello World! It's Gary Jiang. In the flesh.
+        Hello World! It's Gary Jiang. In the flesh.
+        Hello World! It's Gary Jiang. In the flesh.
+        Hello World! It's Gary Jiang. In the flesh.
+      </Text>
+      <Text style={styles.text}>Hi there. The mobile view renders so fast.</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#ffffff'
+  }
 });
